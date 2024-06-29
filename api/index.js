84a1391d7b4844a3d967ff83be237d0e4668e078
manyagaur <manyagaur25@gmail.com>
2024-06-29 23:55:53 +0530

    initial commit

---
+++ api/index.js
@@ -0,0 +1,10 @@
+import Express from "express";
+
+const app= Express()
+import userRoutes from "./routes/users.js";
+
+app.use("/api/users",userRoutes)
+
+app.listen(8800,()=>{
+    console.log("API Working")
+})
